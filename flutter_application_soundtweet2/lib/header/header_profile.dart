d4a0d35244aced907ac9bc03a3a75b29f212826c
kuriki2718281 <kuriki8281@gmail.com>
2021-05-23 11:29:18 +0900

    #6

--- flutter_application_soundtweet2/lib/header/header_profile.dart
+++ flutter_application_soundtweet2/lib/header/header_profile.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
+// import 'package:image_picker/image_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'header_setting.dart';
 
 class Profile extends StatelessWidget {
   final String iconImage;
+
   Profile({Key key, @required this.iconImage}) : super(key: key);
   @override
   Widget build(BuildContext context) {
@@ -28,10 +32,63 @@ class Profile extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: Column(
-          children: [],
+        child: Profiledetail(),
+      ),
+    );
+  }
+}
+
+class Profiledetail extends StatefulWidget {
+  @override
+  _Profiledetail createState() => _Profiledetail();
+}
+
+class _Profiledetail extends State<Profiledetail> {
+  // File _image;
+  // final picker = ImagePicker();
+  // Future getImage() async {
+  //   final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+  //   setState(() {
+  //     if (pickedFile != null) {
+  //       _image = File(pickedFile.path);
+  //     } else {
+  //       print('No image selected.');
+  //     }
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: <Widget>[
+        WidgetA(),
+        WidgetB(),
+      ],
+    ));
+  }
+}
+
+class WidgetA extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Align(
+        alignment: Alignment(0, -0.7),
+        child: CircleAvatar(
+          radius: 60,
+          backgroundColor: Colors.white,
+          backgroundImage: AssetImage('images/IMG_8723.jpeg'),
         ),
       ),
     );
+  }
+}
+
+class WidgetB extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center();
   }
 }
